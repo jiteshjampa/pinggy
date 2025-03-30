90b66a70976f5f710c1037843a30cd100843f6ee
J JITESH REDDY <79923685+jiteshjampa@users.noreply.github.com>
2025-03-30 20:00:38 +0530

    Update client.ts

--- frontend/src/app/api/client.ts
+++ frontend/src/app/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://3.110.55.64:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
